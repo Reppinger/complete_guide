a84773e36b7b48091ac2b38ee32468031711b0cd
Randy Eppinger <r@eppingers.name>
2020-08-25 16:56:19 -0400

    Spacing?

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  @override
   State<StatefulWidget> createState() {
     return _MyAppState();
   }
