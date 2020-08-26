19ad164acaaa4fb7cf6fe4edf0c736e30b7e8db1
Randy Eppinger <randy@eppingerconsulting.com>
2020-08-26 17:57:05 -0400

    Forgot to update analyze-results in store-artifacts step

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  @override
   State<StatefulWidget> createState() {
     return _MyAppState();
   }
