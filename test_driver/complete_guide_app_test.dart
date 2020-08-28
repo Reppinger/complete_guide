b52703631867077d9f4b5b86baf880d2cf15fc58
Randy Eppinger <randy@eppingerconsulting.com>
2020-08-28 15:50:56 -0400

    Completed first two flutter driver tests

--- test_driver/complete_guide_app_test.dart
+++ test_driver/complete_guide_app_test.dart
@@ -3,7 +3,32 @@ import 'package:test/test.dart';
 
 void main(){
   group('Complete Guide App', () {
-    // git final questionTextFinder = find.byValueKey('question');
+    final questionTextFinder = find.byValueKey('question');
+    final answer1RaisedButtonFinder = find.byValueKey('answer1');
+
+    FlutterDriver driver;
+
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    tearDownAll(() async {
+      if (driver != null) {
+        await driver.close();
+      }
+    });
+
+    test('starts at question 1', () async {
+      var question = await driver.getText(questionTextFinder);
+      expect(question, 'What is your favorite color?');
+    });
+
+    test('clicking answer increments question', () async {
+      await driver.tap(answer1RaisedButtonFinder);
+      var question = await driver.getText(questionTextFinder);
+      expect(question, 'What\'s your favorite animal?');
+    });
+
   });
 
 }
