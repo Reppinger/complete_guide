85d8aaf4de85bba8d33ed11e62b62253183d72d5
Randy Eppinger <randy@eppingerconsulting.com>
2020-08-30 15:11:52 -0400

    Formatting & build step labels

--- lib/question.dart
+++ lib/question.dart
@@ -8,10 +8,12 @@ class Question extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      margin: EdgeInsets.all(10),
-      child: Text(questionText, 
-    style: TextStyle(fontSize: 24),
-    textAlign: TextAlign.center,));
+        width: double.infinity,
+        margin: EdgeInsets.all(10),
+        child: Text(
+          questionText,
+          style: TextStyle(fontSize: 24),
+          textAlign: TextAlign.center,
+        ));
   }
 }
